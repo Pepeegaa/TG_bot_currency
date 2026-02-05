226cd73e479f65f48ae62c75e1700bc9037497dd
Pepega213 <kira.gritsaenko@gmail.com>
2026-02-05 20:42:50 +0300

    Change db path

--- db_orm/engine.py
+++ db_orm/engine.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
-DATABASE_URL = "sqlite:///C:/PyTHoN/Pycharm_proj/TG_bot_test.db"
+DATABASE_URL = "sqlite:////opt/TG_bot/db_orm/TG_bot_test.db"
 
 engine = create_engine(
     DATABASE_URL,
